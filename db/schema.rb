01248c325dce293d6f4f2e35dc1a42e079e7167e
Ayush Jain <jain20.ayush@gmail.com>
2020-03-30 16:22:44 +0530

    add_users-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_094647) do
+ActiveRecord::Schema.define(version: 2020_03_30_104642) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
